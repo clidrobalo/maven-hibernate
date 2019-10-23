5efc0b6e82358d3cd942f07c65f02662e21e08c9
clidrobalo <clidrobalo@gmail.com>
2019-10-23 02:30:02 +0100

    Correção erro no ficheiro persistence.xml e criação da Tabela Pessoa.

--- src/main/java/maven_hibernate_2/maven_hibernate_2/HibernateUtil.java
+++ src/main/java/maven_hibernate_2/maven_hibernate_2/HibernateUtil.java
@@ -1,10 +1,28 @@
 package maven_hibernate_2.maven_hibernate_2;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class HibernateUtil {
 
 	public static EntityManagerFactory factory = null;
 	
+	static {
+		init();
+	}
 	
+	private static void init() {
+		try {
+			if(factory == null) {
+				factory = Persistence.createEntityManagerFactory("maven-hibernate-2");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static EntityManager getEntityManager() {
+		return factory.createEntityManager(); //prove a parte de persistencia
+	}
 }
---
+++ src/test/java/maven_hibernate_2/maven_hibernate_2/TesteHibernateUtil.java
@@ -0,0 +1,12 @@
+package maven_hibernate_2.maven_hibernate_2;
+
+import org.junit.jupiter.api.Test;
+
+public class TesteHibernateUtil {
+	
+	@Test
+	public void testeHibernateUtil() {
+		HibernateUtil.getEntityManager();
+	}
+
+}
