a93068c73b0aaedbc86d9f0fc6f7151fefa0d383
clidrobalo <clidrobalo@gmail.com>
2019-10-23 01:40:48 +0100

    correção de erros de dependencias

---
+++ src/main/java/maven_hibernate_2/maven_hibernate_2/HibernateUtil.java
@@ -0,0 +1,10 @@
+package maven_hibernate_2.maven_hibernate_2;
+
+import javax.persistence.EntityManagerFactory;
+
+public class HibernateUtil {
+
+	public static EntityManagerFactory factory = null;
+	
+	
+}
